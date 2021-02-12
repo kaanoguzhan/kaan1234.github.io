da6050da446287ef1d5456d99b047119f06df68e
Kaan Oguzhan <kaan135@gmail.com>
2021-02-12 04:49:29 +0100

    about me page and cart updates

--- js/custom/animateLanguagesChart.js
+++ js/custom/animateLanguagesChart.js
@@ -4,7 +4,6 @@ $(function () {
     var chartHidden = true;
     $(window).scroll(function () {
         var pTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-        console.log(oTop, pTop)
         if ((pTop > oTop) && (chartHidden)) {
             chartHidden = false;
             start_count();
@@ -13,50 +12,67 @@ $(function () {
 });
 
 function start_count() {
-    var ctx = document.getElementById("myChart").getContext('2d');
-
-
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+    var ctx = document.getElementById("languageCart").getContext('2d');
+    var languageCart = new Chart(ctx, {
+        type: 'horizontalBar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['German', 'English', 'Turkish'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [15, 18, 20],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    // 'rgba(75, 192, 192, 0.2)',
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
                 ],
-                borderWidth: 1
+                borderWidth: 1.5
             }]
         },
         options: {
             scales: {
-                yAxes: [{
+                xAxes: [{
                     ticks: {
-                        beginAtZero: true
-                    }
+                        beginAtZero: true,
+                        min: 0,
+                        display: false, //this removed the labels on the x-axis
+                    },
+                    // scaleLabel: {
+                    //     display: true,
+                    //     labelString: "Hours",
+                    // },
                 }]
-            }, animation: {
+            },
+            legend: {
+                display: false
+            },
+            animation: {
                 duration: 12000,
+            }, tooltips: {
+                callbacks: {
+                    label: function (tooltipItem) {
+                        if (tooltipItem.xLabel == 15) {
+                            return "Advanced (C1)";
+                        } else if (tooltipItem.xLabel == 18) {
+                            return "Advanced";
+                        } else if (tooltipItem.xLabel == 20) {
+                            return "Native";
+                        } else {
+                            return tooltipItem.xLabel;
+                        }
+                    }
+                }
             }
         }
     }
     );
 }
-
-
-
-// start_count();
