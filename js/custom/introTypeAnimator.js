d860a3872557392b4f8f8d4450cd1d94be2034ef
Kaan Oguzhan <kaan135@gmail.com>
2021-02-14 05:08:10 +0100

    typing animation

---
+++ js/custom/introTypeAnimator.js
@@ -0,0 +1,9 @@
+var type_options = {
+  strings: [' ',' Welcome To My Personal Blog...', ' Welcome To My Persoensal Page.', ' Welcome To My Personal Page.', 'Welcome To My Personal Page <b>!!</b>'],
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 800,
+};
+
+var typed = new Typed('#intro-title', type_options);
+
